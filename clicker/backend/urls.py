a34551bc853b66ae1d00290c914522589f070fad
Artyom <art-pavlov2000@mail.ru>
2022-05-25 17:25:00 +0500

    Модуль авторизации с REST

--- clicker/backend/urls.py
+++ clicker/backend/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from backend.views import index,user_login,user_logout,register
+from backend.views import UserLogin, UserRegister, index, user_logout
 urlpatterns = [
     path('', index, name='home'),
-    path('login', user_login, name='login'),
+    path('login',UserLogin.as_view(), name='login'),
     path('logout', user_logout, name='logout'),
-    path('register', register, name='register'),
+    path('register', UserRegister.as_view(), name='register'),
 ] 
--- clicker/backend/views.py
+++ clicker/backend/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
 from backend.forms import UserForm
-def index(request):
-	return render(request,'index.html')
-def register(request):
-	if request.method=='POST':
-		form=UserForm(request.POST)
-		if form.is_valid():
-			user=form.save()
-			login(request,user)
-			return redirect('home')
-		return render(request, 'register.html', {'form': form, 'user':request.user})
-	form=UserForm()
-	return render(request, 'register.html', {'form': form, 'user':request.user})	
-def user_login(request):
-	form=UserForm()
-	if request.method=='POST':
-		user=authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-		if user:
-			login(request,user)
-			return redirect('home')
-		return render(request, 'login.html', {'form': form, 'user':request.user, 'invalid':True})	
-	return render(request, 'login.html', {'form': form, 'user':request.user, 'invalid':False})
+from backend.models import Core
+from rest_framework.views import APIView
+class UserRegister(APIView):
+    def get(self, request):
+        form = UserForm()
+        return render(request, 'register.html', {'form': form})
+    def post(self, request):
+        form = UserForm(request.POST) 
+        if form.is_valid(): 
+            user = form.save() 
+            login(request, user)
+
+            user_core = Core(user=user)
+            user_core.save()
+            return redirect('home')
+
+        return render(request, 'register.html', {'form': form})
+
+class UserLogin(APIView):
+    form = UserForm()
+    def get(self, request):
+        return render(request, 'login.html', {'form': self.form})
+
+    def post(self, request):
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password')) 
+        if user!=None:
+            login(request, user) 
+            return redirect('home')
+        return render(request, 'login.html', {'form': self.form, 'invalid': True})
+   
+@login_required
+def index(request):         
+    core = Core.objects.get(user=request.user)
+    return render(request, 'index.html', {'core': core})
 def user_logout(request):
 	logout(request)
 	return redirect('login')
+
